f425026c466e60f95ae023a6c4b5e392da485311
saheedibrahim <saolabram@gmail.com>
2022-06-21 07:14:23 +0100

    first push

---
+++ intro_javascript.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = "Ibrahim Saheed Olanrewaju";
+document.getElementById("height").innerHTML = "5ft";
+document.getElementById("country").innerHTML = "Nigeria";
